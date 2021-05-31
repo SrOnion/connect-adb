329a0df633a08e0a15206e448382d6e4d49e20a7
On1on_ <74428353+SrOnion@users.noreply.github.com>
2021-05-31 19:38:58 -0300

    Add files via upload

--- adb-hacking.py
+++ adb-hacking.py
@@ -1,7 +1,9 @@
 #/bin/usr/env python3
 
+from threading import Thread
 from sys import exit
 from os import system
+from playsound import playsound
 
 v = '\033[1;31m'; a = '\033[1;34m'; cl = '\033[0;0m'
 
@@ -13,7 +15,7 @@ def banner():
     ▐█ ▀█ ██▪ ██ ▐█ ▀█▪    ██▪▐█▐█ ▀█ ▐█ ▌▪█▌▄▌▪██ •█▌▐█▐█ ▀ ▪
     ▄█▀▀█ ▐█· ▐█▌▐█▀▀█▄    ██▀▐█▄█▀▀█ ██ ▄▄▐▀▀▄·▐█·▐█▐▐▌▄█ ▀█▄
     ▐█ ▪▐▌██. ██ ██▄▪▐█    ██▌▐▀▐█ ▪▐▌▐███▌▐█.█▌▐█▌██▐█▌▐█▄▪▐█
-     ▀  ▀ ▀▀▀▀▀• ·▀▀▀▀     ▀▀▀ · ▀  ▀ ·▀▀▀ ·▀  ▀▀▀▀▀▀ █▪·▀▀▀▀ 
+     ▀  ▀ ▀▀▀▀▀• ·▀▀▀▀     ▀▀▀ · ▀  ▀ ·▀▀▀ ·▀  ▀▀▀▀▀▀ █▪·▀▀▀▀   {v}Discord: {a}On1on_#0506
                                                                 {v}Created by {a}On1on{v}
 --------------------------------------------------------------------------------------------------
 
@@ -34,7 +36,7 @@ def banner2():
     ▐█ ▀█ ██▪ ██ ▐█ ▀█▪    ██▪▐█▐█ ▀█ ▐█ ▌▪█▌▄▌▪██ •█▌▐█▐█ ▀ ▪
     ▄█▀▀█ ▐█· ▐█▌▐█▀▀█▄    ██▀▐█▄█▀▀█ ██ ▄▄▐▀▀▄·▐█·▐█▐▐▌▄█ ▀█▄
     ▐█ ▪▐▌██. ██ ██▄▪▐█    ██▌▐▀▐█ ▪▐▌▐███▌▐█.█▌▐█▌██▐█▌▐█▄▪▐█
-     ▀  ▀ ▀▀▀▀▀• ·▀▀▀▀     ▀▀▀ · ▀  ▀ ·▀▀▀ ·▀  ▀▀▀▀▀▀ █▪·▀▀▀▀ 
+     ▀  ▀ ▀▀▀▀▀• ·▀▀▀▀     ▀▀▀ · ▀  ▀ ·▀▀▀ ·▀  ▀▀▀▀▀▀ █▪·▀▀▀▀   {v}Discord: {a}On1on_#0506 
                                                                 {v}Created by {a}On1on{v}
 --------------------------------------------------------------------------------------------------
 
@@ -46,6 +48,16 @@ def banner2():
 
     [{a}00{v}] - exit                                         [{a}99{v}] - return {cl}
     """)
+
+def m(wav):
+    playsound(wav)
+def p(wav):
+    playsound(wav)
+
+monster = Thread(target=m,args=['audios/monster.wav', ])
+monster.start()
+monster = Thread(target=m,args=['audios/monster.wav', ])
+
 system('clear')
 
 def op_banner():
@@ -58,23 +70,53 @@ def op_banner():
         try:
             op = int(input(f'{v} --> :{a}'))
         except:
+            daned = Thread(target=m,args=['audios/daned.wav', ])
+            daned.start()
+            daned = Thread(target=m,args=['audios/daned.wav', ])
             print(f'{v}invalid Command{cl}')
             exit()
         if op == 1:
+            fatalit = Thread(target=p,args=['audios/fatalit.wav', ])
+            fatalit.start()
+            fatalit = Thread(target=p,args=['audios/fatalit.wav', ])
             ip = str(input(f'{v}IP:{a} '))
         elif op == 3:
+            scorp = Thread(target=m,args=['audios/scorp.wav', ])
+            scorp.start()
+            scorp = Thread(target=m,args=['audios/scorp.wav', ])
+            
             apk = str(input(f'{v}Apk directory:{a} '))
         elif op == 6: 
+            finish = Thread(target=m,args=['audios/finish.wav', ])            
+            finish.start()
+            finish = Thread(target=m,args=['audios/finish.wav', ])
+            
             dir = str(input(f'{v}package name:{a} '))
         elif op == 00:
+            win = Thread(target=m,args=['audios/win.wav', ])            
+            win.start()
+            win = Thread(target=m,args=['audios/win.wav', ])
+            
             print(f'{v}Exiting..')
             exit()
         elif op == 99:
+            kill = Thread(target=m,args=['audios/kill.wav', ])            
+            kill.start()
+            kill = Thread(target=m,args=['audios/kill.wav', ])
+            
             system('clear')
             op_banner2()
         if op == 9:
+            scorp = Thread(target=m,args=['audios/scorp.wav', ])
+            scorp.start()
+            scorp = Thread(target=m,args=['audios/scorp.wav', ])
+            
             dir = str(input(f'{v}package name: {a}'))
         if op > 9:
+            daned = Thread(target=m,args=['audios/daned.wav', ])
+            daned.start()
+            daned = Thread(target=m,args=['audios/daned.wav', ])
+            
             print(f'{v}invalid Command{cl}')
             exit()
         dic = {1: f'adb connect {ip}:5555', 2: 'adb disconnect', 3: f'adb install {apk}', 4: 'adb devices', 5: 'adb shell', 6: f'adb uninstall {dir}', 7: 'adb shell pm list packages -3 -f', 8: 'scrcpy', 9: f'adb shell am start -n {dir} && adb shell monkey -p {dir} -c android.intent.category.LAUNCHER 1'}
@@ -88,12 +130,23 @@ def op_banner2():
         op = int(input(f'{v} --> :{a}'))
 
         if op == 18:
+            finish = Thread(target=m,args=['audios/finish.wav', ])
+            finish.start()
+            finish = Thread(target=m,args=['audios/finish.wav', ])
+            
             com = str(input('your command: '))
             system(com)
         elif op == 00:
+            win = Thread(target=m,args=['audios/win.wav', ])
+            win.start()
+            win = Thread(target=m,args=['audios/win.wav', ])
+            
             print('Exiting..')
             exit()
         elif op == 99:
+            kill = Thread(target=m,args=['audios/kill.wav', ])
+            kill.start()
+            kill = Thread(target=m,args=['audios/kill.wav', ])
             system('clear')
             op_banner()
         dic = {10: 'adb dumpsys battery', 11: 'adb logcat', 12: 'adb shell wm size', 13: 'adb shell netstat', 14: 'adb shell ps', 15: 'adb get-serialno', 16: 'adb root ', 17: 'adb reboot', 18: f'{com}'}
---
+++ install.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+sudo apt install adb scrcpy python3-gst-1.0 -y
+sudo rm -rf install.sh
